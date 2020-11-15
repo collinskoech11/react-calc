f6be9ac2cced71ad003586aa3ece541f1ef7644f
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-11-15 23:15:06 +0300

    Update App.js

--- src/component/App.js
+++ src/component/App.js
@@ -17,7 +17,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="component-app">
+      <div className="component-app" id="">
         <Display value={this.state.next || this.state.total || "0"} />
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
